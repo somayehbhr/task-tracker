3866f4231450b500274e0f36e3437b632fdbc383
Somayeh Bahiraie <somayebhr@gmail.com>
2021-05-30 12:45:53 +0430

    feat: added useLocation to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
 import propTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 import Button from "./Button";
 
 const Header = ({ title, onAdd, showAdd }) => {
 	// const onClick = () => {
 	// 	console.log('click');
 	// };
+	const location = useLocation();
+
 	return (
 		<header className="header">
 			<h1>{title}</h1>
-			<Button color={showAdd ? "red" : "green"} text={showAdd ? "Close" : "Add"} onClick={onAdd} />
+			{location.pathname === "/" && (
+				<Button color={showAdd ? "red" : "green"} text={showAdd ? "Close" : "Add"} onClick={onAdd} />
+			)}
 		</header>
 	);
 };
